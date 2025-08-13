b431a6d435760ee8b7d5c7eb1da97aaae6a0842e
Nana <nackawat@gmail.com>
2025-08-13 17:33:48 +0700

    nanacommit

--- nana_ws/build/nana_core/prefix_override/sitecustomize.py
+++ nana_ws/build/nana_core/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/nadeem/nana_project/nana_ws/install/nana_core'
+    sys.prefix = sys.exec_prefix = '/home/nnn/nana_project/nana_ws/install/nana_core'
